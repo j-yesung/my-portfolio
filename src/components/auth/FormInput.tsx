e3730ffbdbc6dbec48a1a405eeb499299f3677dd
j-yesung <hisung-ah@kakao.com>
2024-04-22 20:00:18 +0900

    ✨ update: Form 공용 input 컴포넌트 생성

---
+++ src/components/auth/FormInput.tsx
@@ -0,0 +1,23 @@
+import { FieldValues, UseFormRegister } from 'react-hook-form';
+
+interface FormInputProps {
+  label: string;
+  name: string;
+  type: string;
+  register: UseFormRegister<FieldValues>;
+  required: boolean;
+  pattern?: RegExp;
+  errorMessage?: string;
+}
+
+const FormInput = ({ label, name, type, register, required, pattern, errorMessage }: FormInputProps) => {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input id={name} type={type} {...register(name, { required, pattern })} />
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default FormInput;
